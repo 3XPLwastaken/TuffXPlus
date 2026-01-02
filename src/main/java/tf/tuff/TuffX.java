2205224ac427d31856a05c8b57fa65e912695a03
Potato. <105754685+dragon731012@users.noreply.github.com>
2026-01-02 13:26:16 -1000

    Work in progress for main file

--- src/main/java/tf/tuff/TuffX.java
+++ src/main/java/tf/tuff/TuffX.java
@@ -1,1 +1,108 @@
+package tf.tuff.y0;
 
+import com.github.retrooper.packetevents.PacketEvents;
+import com.github.retrooper.packetevents.event.PacketListenerPriority;
+import io.github.retrooper.packetevents.factory.spigot.SpigotPacketEventsBuilder;
+import org.bukkit.Chunk;
+import org.bukkit.ChunkSnapshot;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.messaging.PluginMessageListener;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.event.block.BlockPhysicsEvent;
+import org.bukkit.event.block.BlockExplodeEvent;
+import org.bukkit.event.block.BlockFromToEvent;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import java.util.concurrent.*;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import com.google.common.cache.*;
+import java.util.logging.Level;
+import java.lang.reflect.Method; 
+import it.unimi.dsi.fastutil.objects.*;
+import it.unimi.dsi.fastutil.shorts.*;
+import it.unimi.dsi.fastutil.bytes.*;
+
+public class TuffX extends JavaPlugin implements Listener, PluginMessageListener {
+
+    private ServerRegistry serverRegistry;
+
+    @Override
+    public void onLoad() {
+        PacketEvents.setAPI(SpigotPacketEventsBuilder.build(this));
+        PacketEvents.getAPI().getSettings().reEncodeByDefault(false)
+                .checkForUpdates(false)
+                .bStats(true);
+        PacketEvents.getAPI().load();
+    }
+
+    @Override
+    public void onEnable() {
+        PacketEvents.getAPI().init();
+
+        saveDefaultConfig();
+        
+        PacketEvents.getAPI().getEventManager().registerListener(
+            new ChunkPacketListener(this), PacketListenerPriority.NORMAL
+        );
+
+        getServer().getMessenger().registerOutgoingPluginChannel(this, CH);
+        getServer().getMessenger().registerIncomingPluginChannel(this, CH, this);
+        getServer().getPluginManager().registerEvents(this, this);
+
+        if (getConfig().getBoolean("registry.enabled", false)) {
+            String url = getConfig().getString("registry.server-url");
+            String ws = getConfig().getString("registry.server");
+
+            if (!ws.isEmpty() && !ws.equals("wss://urserverip.net")) {
+                serverRegistry = new ServerRegistry(this, url, ws);
+                serverRegistry.connect();
+            }
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        if (serverRegistry != null) {
+            serverRegistry.disconnect();
+            serverRegistry = null;
+        }
+
+        PacketEvents.getAPI().terminate();
+    }
+
+    @Override
+    public void onPluginMessageReceived(String channel, Player player, byte[] message) {
+        if (!player.isOnline()) return;
+      
+        if (channel.equals("eagler:below_y0") Y0Plugin.handlePacket(player,message):
+    }
+  
+    private void lfe() {
+        getLogger().info("");
+        getLogger().info("████████╗██╗   ██╗███████╗ ███████╗ ██╗  ██╗");
+        getLogger().info("╚══██╔══╝██║   ██║██╔════╝ ██╔════╝ ╚██╗██╔╝");
+        getLogger().info("   ██║   ██║   ██║██████╗  ██████╗   ╚███╔╝ ");
+        getLogger().info("   ██║   ██║   ██║██╔═══╝  ██╔═══╝   ██╔██╗ ");
+        getLogger().info("   ██║   ╚██████╔╝██║      ██║      ██╔╝╚██╗");
+        getLogger().info("   ╚═╝    ╚═════╝ ╚═╝      ╚═╝      ╚═╝  ╚═╝");
+        getLogger().info("");
+        getLogger().info("Below y0 and TuffX programmed by Potato");
+    }
+}
