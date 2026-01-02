6cc66c099b982db11f0f5026753d7f3dbc1ab68d
Potato. <105754685+dragon731012@users.noreply.github.com>
2026-01-02 12:26:43 -1000

    Add files via upload

--- src/main/java/tf/tuff/viablocks/version/legacy/LegacyAdapter.java
+++ src/main/java/tf/tuff/viablocks/version/legacy/LegacyAdapter.java
@@ -1,1 +1,37 @@
+package com.viablocks.viaBlocks.version.legacy;
 
+import com.viablocks.viaBlocks.version.VersionAdapter;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import java.util.EnumSet;
+
+public class LegacyAdapter implements VersionAdapter {
+
+    @Override
+    public EnumSet<Material> getModernMaterials() {
+        return EnumSet.noneOf(Material.class);
+    }
+
+    @Override
+    public String getBlockDataString(Block block) {
+        return "minecraft:" + block.getType().name().toLowerCase();
+    }
+
+    @Override
+    public String getMaterialKey(Material material) {
+        return "minecraft:" + material.name().toLowerCase();
+    }
+
+    @Override
+    public int getClientViewDistance(Player player) {
+        return player.getServer().getViewDistance();
+    }
+
+    @Override
+    public void giveCustomBlocks(Player player) {
+        player.getInventory().addItem(new ItemStack(Material.STONE));
+        player.getInventory().addItem(new ItemStack(Material.COBBLESTONE));
+    }
+}
