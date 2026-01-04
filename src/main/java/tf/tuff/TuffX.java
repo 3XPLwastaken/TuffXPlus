8669a89434b72e3f4480912440fb290c80c0d4f4
Potato. <105754685+dragon731012@users.noreply.github.com>
2026-01-03 21:20:04 -0800

    final

--- src/main/java/tf/tuff/TuffX.java
+++ src/main/java/tf/tuff/TuffX.java
@@ -149,8 +149,8 @@ public class TuffX extends JavaPlugin implements Listener, PluginMessageListener
     
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (command.getName().equalsIgnoreCase("tuffx") return TuffXCommand(sender, command, label, args); 
-        if (command.getName().equalsIgnoreCase("viablocks") return viaBlocksPlugin.onTuffXCommand(sender, command, label, args);
+        if (command.getName().equalsIgnoreCase("tuffx")) return TuffXCommand(sender, command, label, args); 
+        if (command.getName().equalsIgnoreCase("viablocks")) return viaBlocksPlugin.onTuffXCommand(sender, command, label, args);
     } 
 
     @Override
